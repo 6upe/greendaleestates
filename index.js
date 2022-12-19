5c96e3ad3bab4d795af5be93154c47bfba3056d0
bupe <katongobupe444@gmail.com>
2022-12-19 05:49:34 -0800

    demo admin link

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ var uEmail = document.getElementById("user_email").value;
 var uPassword = document.getElementById("user_password").value;
 
 function auth(){
-    if(uEmail == "greendale@email.com" && uPassword == 1234){
-        window.location = "admin.html";
-    }else{
-        alert(uEmail + ' and ' + uPassword);
+    // if(uEmail == "greendale@email.com" && uPassword == 1234){
+        // window.location = "admin.html";
+    // }else{
+        // alert(uEmail + ' and ' + uPassword);
         // console.log('email: ' + uEmail);
         // console.log('password: ' + uPassword);
-    }
+    // }
 }
